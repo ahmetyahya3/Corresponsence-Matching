5d33a7a9e7542ed887ad400d0dc350f6222584c5
ahmetyahya3 <ahmetyahyasenel3@gmail.com>
2023-09-04 23:14:54 +0300

    added normalization

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Users/ahmet/Desktop/Tasks/Corresponsence Matching
+# Install script for directory: C:/Users/Arg/Documents/Repositories/Corresponsence-Matching
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files/Workspace")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files/corresponsenceMatching")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/ahmet/Desktop/Tasks/Corresponsence Matching/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Arg/Documents/Repositories/Corresponsence-Matching/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@
 using namespace std;
 using namespace cv;
 
-double dMapper(double value, double start, double end, double mappedStart, double mappedEnd){//it maps the segments for doubles
-    return (value-start)*(mappedEnd-mappedStart)/(end-start)+mappedStart;
-}
-
 vector<vector<double>> readCSV(string fileName){
     ifstream inFile("../../src/"+fileName);
     vector<vector<double>> matrix;
@@ -35,21 +31,26 @@ int main(){
     vector<vector<double>> leftUwpMap = readCSV("left_uwp_map.csv");
 
     int rows = rightUwpMap.size(), colums = rightUwpMap[0].size();
-    cout << rows << " " << colums << endl;
 
-    Mat mat(rows, colums, CV_8UC1);
+    Mat rightUwp(rows, colums, CV_32F);
+    Mat leftUwp(rows, colums, CV_32F);
     
-    double mak = 0;
-    double mik = 0;
-
     for(int i = 0; i < rows; i++){
         for(int j = 0; j < colums; j++){
-            mak = max(mak, rightUwpMap[i][j]-leftUwpMap[i][j]);
-            mik = min(mik, rightUwpMap[i][j]-leftUwpMap[i][j]);
-            mat.at<uchar>(i, j) = dMapper(abs(leftUwpMap[i][j]-rightUwpMap[i][j]), 0, 6.8, 0, 255);
+            leftUwp.at<float>(i, j) = leftUwpMap[i][j];
+            rightUwp.at<float>(i, j) = rightUwpMap[i][j];
         }
     }
-    cout << mak << " " << mik << endl;
-    imshow("deneme", mat);
+
+    Mat normalizedRightUwp(rows, colums, CV_32F);
+    Mat normalizedLeftUwp(rows, colums, CV_32F);
+
+    normalize(rightUwp, normalizedRightUwp, 0, 1.0, NORM_MINMAX, CV_32F);
+    normalize(leftUwp, normalizedLeftUwp, 0, 1.0, NORM_MINMAX, CV_32F);
+
+    imshow("normilezedRight", normalizedRightUwp);
+    imshow("normilezedLeft", normalizedLeftUwp);
+
+
     waitKey(0);
 }
